85828e48159ef3922728b7d5b71964eb2b66ca83
davidforgot7727-png <davidforgot7727@gmail.com>
2026-09-02 18:24:01 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const VERSION='5.6.0';
+const VERSION='5.6.1';
 const SHELL_CACHE=`fukuoka-shell-${VERSION}`;
 const RUNTIME_CACHE=`fukuoka-runtime-${VERSION}`;
 const APP_SHELL=[
   './',
   './index.html',
-  './manifest.json?v=560',
+  './manifest.json?v=561',
   './icons/icon-192.png',
   './icons/icon-512.png',
   './icons/icon-maskable-512.png',
