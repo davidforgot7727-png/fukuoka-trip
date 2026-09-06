64dcd1bdd43534d51b3208a8544d5f2e9e97555e
Derek <davidforgot7727@gamil.com>
2026-09-06 15:51:11 +0800

    feat: allow public coupon browsing and creation with shared sync

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const VERSION='5.8.2';
+const VERSION='5.8.3';
 const SHELL_CACHE=`fukuoka-shell-${VERSION}`;
 const RUNTIME_CACHE=`fukuoka-runtime-${VERSION}`;
 const APP_SHELL=[
   './',
   './index.html',
-  './manifest.json?v=582',
+  './manifest.json?v=583',
   './icons/icon-192.png',
   './icons/icon-512.png',
   './icons/icon-maskable-512.png',
